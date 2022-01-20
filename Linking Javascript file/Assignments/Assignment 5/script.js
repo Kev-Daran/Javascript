b459d8da861555a03f0e207ce45eb5fa440da444
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-20 14:23:04 +0530

    Add files via upload

---
+++ Linking Javascript file/Assignments/Assignment 5/script.js
@@ -0,0 +1,17 @@
+const country = "India";
+const continent = "Asia";
+let population = 1400;
+
+const isIsland = false;
+let language;
+
+language = 'Hindi';
+
+console.log(population / 2, population + 1);
+
+console.log(population > 6);
+console.log(population < 33);
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+console.log(description);
