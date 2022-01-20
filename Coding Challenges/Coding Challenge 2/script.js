9e2fbd5b53fdfe0b3f872275361ca85accde8dee
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-20 18:16:39 +0530

    Add files via upload

---
+++ Coding Challenges/Coding Challenge 2/script.js
@@ -0,0 +1,10 @@
+const johnBMI = 27.6;
+const markBMI = 25.3;
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})`);
+}
+
+else {
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})`);
+}
