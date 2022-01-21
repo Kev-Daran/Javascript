2c0dd5982aa6098f21469a9642c31b9adb0ea28d
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-21 19:29:26 +0530

    Add files via upload

---
+++ Linking Javascript file/Assignments/Assignment 9/script.js
@@ -0,0 +1,23 @@
+language = 'ooga';
+
+switch (language) {
+    case 'Mandarin':
+    case 'Chinese':
+        console.log(`Most number of native speakers`);
+        break;
+
+    case 'Spanish':
+        console.log(`2nd place in number of native speakers`);
+        break;
+
+    case 'English':
+        console.log(`3rd place`);
+        break;
+
+    case 'Hindi':
+        console.log(`Number 4`);
+        break;
+
+    default:
+        console.log(`Great language too :D`);
+}
