94c6539624d3fb4f7fe85eca13b6a25e1eea5cf5
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-22 10:46:12 +0530

    Add files via upload

---
+++ Fundamentals 2/Assignments/Assignment 1/script.js
@@ -0,0 +1,8 @@
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+
+description = describeCountry('India', 1400, 'New Delhi');
+
+console.log(description);
