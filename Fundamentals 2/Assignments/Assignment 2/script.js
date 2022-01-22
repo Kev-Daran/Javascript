0e0a2bcdcc08f651a0c5241701b3f04268254f44
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-22 11:00:19 +0530

    Add files via upload

---
+++ Fundamentals 2/Assignments/Assignment 2/script.js
@@ -0,0 +1,9 @@
+const percentageOfWorld1 = function (population) {
+    return (population / 79);
+}
+
+country1 = 1400;
+country2 = 1600;
+country3 = 6;
+
+console.log(percentageOfWorld1(country1), percentageOfWorld1(country2), percentageOfWorld1(country3));
