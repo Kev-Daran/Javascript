bbb556901bee65b2bf911d97b8ef9a8fd72bac1e
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-23 20:29:11 +0530

    Add files via upload

---
+++ Fundamentals 2/Assignments/Assignment 5/script.js
@@ -0,0 +1,6 @@
+const percentageOfWorld = population => population / 79;
+
+const populations = [1400, 1440, 6, 330];
+
+console.log(percentageOfWorld(populations[0]), percentageOfWorld(populations[1]), percentageOfWorld(populations[2]), percentageOfWorld(populations[3]));
+
