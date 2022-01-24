34206577abeb1890bf949bd966c25e873ac03eb1
Dev Karan <81677957+Kev-Daran@users.noreply.github.com>
2022-01-24 11:31:42 +0530

    Add files via upload

---
+++ Fundamentals 2/Assignments/Assignment 7/script.js
@@ -0,0 +1,9 @@
+const myCountry = {
+    country: 'India',
+    capital: 'New Delhi',
+    language: 'Hindi',
+    population: 1400,
+    neighbours: ['China', 'Pakistan', 'Nepal']
+};
+
+console.log(myCountry);
